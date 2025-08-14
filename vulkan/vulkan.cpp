95db24d9f165e2ad29bd71701b5ebd556b884756
igors <igors@A520>
2025-08-14 23:22:34 +0300

    Add project files.

---
+++ vulkan/vulkan.cpp
@@ -0,0 +1,103 @@
+﻿#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+class Triangle {
+public:
+	void run(){
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+}
+private:
+	
+	GLFWwindow* window;
+	const uint32_t WIDTH = 800;
+	const uint32_t HEIGHT = 600;
+	
+	VkInstance instance;
+
+	void initWindow() {
+		glfwInit();
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+		createInstance();
+	}
+	
+
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
+
+	
+
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtentionCount = 0;
+		const char** glfwExtentions;
+
+		glfwExtentions = glfwGetRequiredInstanceExtensions(&glfwExtentionCount);
+
+		createInfo.enabledExtensionCount = glfwExtentionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtentions;
+
+		createInfo.enabledLayerCount = 0;
+		VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		uint32_t extentionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extentionCount, nullptr);
+
+		std::vector<VkExtensionProperties> extentions(extentionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extentionCount, extentions.data());
+
+		std::cout << "avaible extensions:\n";
+
+		for (const auto& extension : extentions) {
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+	}
+
+	void cleanup() {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+};
+
+int main() {
+	Triangle app;
+
+	try {
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
